5f95d9c1cbaa5fffc0f6d6eef0ab7b941cbf7842
JoseCarlitosGordo <jose.gordo0806@gmail.com>
2025-11-14 16:23:25 +1030

    set delay for timer so that it doesn't run every microsecond lmfao

--- main.py
+++ main.py
@@ -3,11 +3,14 @@ from constants import SCREEN_HEIGHT, SCREEN_WIDTH
 from logger import log_state
 def main():
     print(f"Starting Asteroids! with pygame version: {pygame.version.ver}")
-    pygame.init()
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
-    isRunning = True
+
+    #initialising pygame logic
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    timer = pygame.time.Clock()
+    dt = 0
 
     #adds event listener to the close button at the top right. 
     for event in pygame.event.get():
@@ -15,11 +18,17 @@ def main():
             return
     
     #game loop
-    
+    isRunning = True
     while isRunning:
         log_state()
+        #sets the fill of the frame to a black colour 
         screen.fill("black")
+        #refreshes the game's screen
         pygame.display.flip()
+        #ticks timer every 1/60th seconds, pausing the game for that amount of time
+        last_called = timer.tick(60)
+        dt = last_called/1000
+    
 
 if __name__ == "__main__":
     main()
