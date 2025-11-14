a784a8548a974dd7ee091e198cf2fc8fba29394e
JoseCarlitosGordo <jose.gordo0806@gmail.com>
2025-11-14 17:00:52 +1030

    Got movement and rotation working pretty well

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
 from logger import log_state
+from player import Player
 def main():
     print(f"Starting Asteroids! with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
@@ -12,22 +13,27 @@ def main():
     timer = pygame.time.Clock()
     dt = 0
 
-    #adds event listener to the close button at the top right. 
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            return
+    
     
     #game loop
     isRunning = True
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
     while isRunning:
-        log_state()
-        #sets the fill of the frame to a black colour 
-        screen.fill("black")
-        #refreshes the game's screen
-        pygame.display.flip()
         #ticks timer every 1/60th seconds, pausing the game for that amount of time
         last_called = timer.tick(60)
+        #adds event listener to the close button at the top right. 
         dt = last_called/1000
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        log_state()
+        player.update(dt)
+        #sets the fill of the frame to a black colour 
+        screen.fill("black")
+        player.draw(screen)
+        #refreshes the game's screen
+        pygame.display.flip()
+       
     
 
 if __name__ == "__main__":
