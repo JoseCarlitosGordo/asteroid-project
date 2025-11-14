09ae79fe258e40b4a3beaa4d72052a0c9ac53d25
JoseCarlitosGordo <jose.gordo0806@gmail.com>
2025-11-14 16:15:44 +1030

    created basic game loop and got the close button to work

--- main.py
+++ main.py
@@ -3,8 +3,23 @@ from constants import SCREEN_HEIGHT, SCREEN_WIDTH
 from logger import log_state
 def main():
     print(f"Starting Asteroids! with pygame version: {pygame.version.ver}")
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+    isRunning = True
+
+    #adds event listener to the close button at the top right. 
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            return
+    
+    #game loop
+    
+    while isRunning:
+        log_state()
+        screen.fill("black")
+        pygame.display.flip()
 
 if __name__ == "__main__":
     main()
