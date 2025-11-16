7033f3e2b8796641ff492bcfb44a52a1e551ac97
JoseCarlitosGordo <jose.gordo0806@gmail.com>
2025-11-16 17:10:35 +1030

    randomised shape of asteroids (its a lil scuffed)

--- asteroid.py
+++ asteroid.py
@@ -2,20 +2,43 @@ from circleshape import CircleShape
 from constants import LINE_WIDTH, ASTEROID_MIN_RADIUS
 import pygame
 import random
+import math
 from logger import log_event
 class Asteroid(CircleShape):
     def __init__(self, x, y, radius, speed = 0):
         super().__init__(x, y, radius)
-        self.rotation = self.velocity.x
+        self.angle = 0.0
         self.speed = speed
+        self.local_verts = self.generate_shape()
     def draw(self, screen):
-        pygame.draw.circle(screen, "white", self.position, self.radius, LINE_WIDTH)
-
+        #pygame.draw.circle(screen, "white", self.position, self.radius, LINE_WIDTH)
+        ca = math.cos(math.radians(self.angle))
+        sa = math.sin(math.radians(self.angle))
+        px, py = self.position.x, self.position.y
+        pts = []
+        for x, y in self.local_verts:
+            rx = x * ca - y * sa
+            ry = x * sa + y * ca
+            pts.append((px + rx, py + ry))  # offset to world
+        pygame.draw.polygon(screen, "white", pts, LINE_WIDTH)
+    
+    def generate_shape(self):
+        r = random
+        verts = []
+        points = 16
+        lumpiness = 0.3
+        for i in range(points):
+            theta = (2 * math.pi * i) / points
+            rr = self.radius * (1 + r.uniform(-lumpiness, lumpiness))
+            x = rr * math.cos(theta)
+            y = rr * math.sin(theta)
+            verts.append((x, y))  
+        return verts    
     def update(self, dt):
         #ties the speed of the object to the screen's framerate (every 0.017 seconds)
         self.speed += 0.01
         self.position += self.velocity * dt *self.speed
-        self.rotation *= dt
+        
     
 
     def split(self): 
